137f8065c8519bdedb011a8d3dbf6a9b246fd5f6
zhenlychen <zhenlychen@foxmail.com>
2018-01-09 00:05:09 +0800

    :construction: update a example
    and modify the clientSecret

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 
 const Violet = require('./lib/violet-sdk')
 module.exports = Violet
+
+/**
+ * @example
+ * let violet = new Violet({
+ *  clientId: 'abcde',
+ *  clientKey: 'abcde',
+ *  server: 'https://oauth.xmatrix.studio/api/v2/'
+ * })
+ * let token = violet.getToken(code)
+ */
